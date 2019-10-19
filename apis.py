80b4358286fb57b87c03de2d25f5ff9ba99844f5
aquib <aquib@ensurity.in>
2019-10-20 00:14:54 +0530

    UPDATE: apis working now

--- apis.py
+++ apis.py
@@ -1,11 +1,22 @@
-from flask import Flask, request, url_for
+from flask import Flask, request, url_for, jsonify
 from flask_pymongo import PyMongo 
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/vogohelmetsdata'
 mongo = PyMongo(app)
 
-@app.route('/mongo', methods=['GET'])
-def get_all_docs(data):
-  doc = mongo.db.totalhelmetdata.insert({'abcd':'abcd'})
-  return "inserted"
+@app.route('/mongo', methods=['GET','POST'])
+def get_all_docs():
+  if (request.method == 'POST') :
+    some_json = request.get_json()
+    doc = mongo.db.totalhelmetdata.find_one({'centreId' : some_json['centreId']},{'_id' : 0})
+    totalRidesCompleted = doc['totalRidesCompleted'] + 1
+    if (some_json['helmetReturned']) :
+      totalHelmetsReturned = doc['totalHelmetsReturned'] + 1
+    else : 
+      totalHelmetsReturned = doc['totalHelmetsReturned']
+    myQuery = {"centreId" : some_json['centreId']}
+    doc = mongo.db.totalhelmetdata.update_one(myQuery, {"$set" : {"totalHelmetsReturned" : totalHelmetsReturned, "totalRidesCompleted" : totalRidesCompleted}})
+    return jsonify({"message":"Data updated"})
+  else :
+    return jsonify({"message":"Please pass data"})
